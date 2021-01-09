d0d58f8561ec13e49d8a5862d7ff599d591959e7
Vinícius Borges França <viniciusborgesf@outlook.com>
2021-01-09 17:07:13 -0300

    Added Person create operation

---
+++ src/main/java/one/digital/innovation/personapi/entity/Phone.java
@@ -0,0 +1,25 @@
+package one.digital.innovation.personapi.entity;
+
+import lombok.*;
+import one.digital.innovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
---
+++ src/main/java/one/digital/innovation/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package one.digital.innovation.personapi.service;
+
+import one.digital.innovation.personapi.dto.MessageResponseDTO;
+import one.digital.innovation.personapi.entity.Person;
+import one.digital.innovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
