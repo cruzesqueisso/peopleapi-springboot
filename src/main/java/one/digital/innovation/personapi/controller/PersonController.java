3e55124f4cdde37152f0e7c88888a3db7109722d
Vinícius Borges França <viniciusborgesf@outlook.com>
2021-01-09 19:19:26 -0300

    Developed unit tests for create person feature

--- src/main/java/one/digital/innovation/personapi/controller/PersonController.java
+++ src/main/java/one/digital/innovation/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package one.digital.innovation.personapi.controller;
 
+import lombok.AllArgsConstructor;
 import one.digital.innovation.personapi.dto.request.PersonDTO;
 import one.digital.innovation.personapi.dto.response.MessageResponseDTO;
 import one.digital.innovation.personapi.exception.PersonNotFoundException;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
---
+++ src/test/java/one/digital/innovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,46 @@
+package one.digital.innovation.personapi.service;
+
+import one.digital.innovation.personapi.dto.request.PersonDTO;
+import one.digital.innovation.personapi.dto.response.MessageResponseDTO;
+import one.digital.innovation.personapi.entity.Person;
+import one.digital.innovation.personapi.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digital.innovation.personapi.utils.PersonUtils.createFakeDTO;
+import static one.digital.innovation.personapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO succesMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, succesMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
